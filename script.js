8bf6f3901df1c4944745d963080c525205f5aca2
Diksha-Bule23 <dikshabule13@gmail.com>
2026-05-01 00:24:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// Simple animation on scroll
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(card => {
+    let position = card.getBoundingClientRect().top;
+    let screen = window.innerHeight;
+
+    if(position < screen - 100){
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
+
+function toggleMenu() {
+  const menu = document.getElementById("navLinks");
+  menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+// Simple animation on scroll
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let position = card.getBoundingClientRect().top;
+    let screen = window.innerHeight;
+
+    if (position < screen - 100) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
+
+document.querySelectorAll('.contact-info, .contact-form').forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(50px)";
+
+  setTimeout(() => {
+    el.style.transition = "1s";
+    el.style.opacity = 1;
+    el.style.transform = "translateY(0)";
+  }, 300);
+});
+
+
+function goToSolutions() {
+  document.getElementById("solutions").scrollIntoView({
+    behavior: "smooth"
+  });
+}
